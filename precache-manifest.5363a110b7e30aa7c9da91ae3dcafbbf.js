62bd3ba19c41c56300c357f7e43d526d3a639684
krbaio3 <krbaio3@gmail.com>
2019-03-16 22:39:39 +0100

    Updates

---
+++ precache-manifest.5363a110b7e30aa7c9da91ae3dcafbbf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4176439faf219b1fa10a",
+    "url": "/react-poc/static/css/main.9fbb84de.chunk.css"
+  },
+  {
+    "revision": "4176439faf219b1fa10a",
+    "url": "/react-poc/static/js/main.4176439f.chunk.js"
+  },
+  {
+    "revision": "587f3130dd3af55dece2",
+    "url": "/react-poc/static/js/runtime~main.587f3130.js"
+  },
+  {
+    "revision": "cfbda0e9061432ca6501",
+    "url": "/react-poc/static/js/2.cfbda0e9.chunk.js"
+  },
+  {
+    "revision": "519f077316dfdce2505a9d83e07d4e51",
+    "url": "/react-poc/index.html"
+  }
+];
